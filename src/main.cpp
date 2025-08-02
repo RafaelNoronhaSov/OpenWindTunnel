5ab8bb1fea9912e16835215ace22e5e4272a0bc0
RafaelNoronhaSov <rafaelnoronhadeoliveirasoveral@gmail.com>
2025-08-02 13:22:02 -0300

    chore: add project configuration and tooling

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "WIP";
+	return 0;
+}
---
+++ toolchain.cmake
@@ -0,0 +1,2 @@
+set(VCPKG_TARGET_TRIPLET "x64-windows-static" CACHE STRING "")
+include("${CMAKE_CURRENT_LIST_DIR}/external/vcpkg/scripts/buildsystems/vcpkg.cmake")
